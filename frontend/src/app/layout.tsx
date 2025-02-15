fdcd6f99e81f77b55336b71b302cec28a34657cb
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2025-02-15 02:39:38 -0300

    Install: Daisy ui, React icons,  React hook form
    . Config Daisy ui
    - Pruebas Dasiy ui
    - install: react Icons, React hook Form

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+
+import { ToastContainer } from 'react-toastify';
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,11 +26,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <ToastContainer />
       </body>
     </html>
   );
