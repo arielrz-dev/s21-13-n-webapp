018a5e5a0ba5f07f8995ce37a694781fd32d96f9
arielrz-dev <ruizarmando.dev@gmail.com>
2025-02-18 22:40:28 -0300

    added config dotenv

--- ecommerceManagementAPI/src/main/java/com/app/ecommerceManagementAPI/EcommerceManagementApiApplication.java
+++ ecommerceManagementAPI/src/main/java/com/app/ecommerceManagementAPI/EcommerceManagementApiApplication.java
@@ -1,5 +1,6 @@
 package com.app.ecommerceManagementAPI;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommerceManagementApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
+		System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+		System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+
 		SpringApplication.run(EcommerceManagementApiApplication.class, args);
 	}
 
--- ecommerceManagementAPI/src/test/java/com/app/ecommerceManagementAPI/EcommerceManagementApiApplicationTests.java
+++ ecommerceManagementAPI/src/test/java/com/app/ecommerceManagementAPI/EcommerceManagementApiApplicationTests.java
@@ -1,13 +1,18 @@
 package com.app.ecommerceManagementAPI;
 
-import org.junit.jupiter.api.Test;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EcommerceManagementApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @BeforeAll
+  public static void setup() {
+    Dotenv dotenv = Dotenv.load();
+    System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
+    System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+    System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
 
+  }
 }
