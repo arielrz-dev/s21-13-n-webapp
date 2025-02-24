ec2af3abed51b7daade0e88b933ca4d3608ee10e
Claudia Ramos <cramos@fi.uba.ar>
2025-02-24 11:45:33 -0300

    Se agregaron anotaciones para la entidad User

--- ecommerceManagementAPI/src/main/java/com/app/ecommerce_management_api/model/User.java
+++ ecommerceManagementAPI/src/main/java/com/app/ecommerce_management_api/model/User.java
@@ -1,6 +1,10 @@
 package com.app.ecommerce_management_api.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,7 +12,11 @@ import java.util.List;
 
 @Entity
 @Table(name = "users")
-public class    User {
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "id", nullable = false)
@@ -25,35 +33,4 @@ public class    User {
   private List<Address> addresses = new ArrayList<>();
 
 
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getRole() {
-    return role;
-  }
-
-  public void setRole(String role) {
-    this.role = role;
-  }
 }
