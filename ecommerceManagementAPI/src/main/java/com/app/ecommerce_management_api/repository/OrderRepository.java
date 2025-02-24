61460a5c44df48c79794ff4f135f430b961f1203
Claudia Ramos <cramos@fi.uba.ar>
2025-02-24 15:54:52 -0300

    Revert "Merge pull request #11 from No-Country-simulation/feature/product"
    This reverts commit fe72027c8aeb1e68922a81afe38c08b4c2c96026, reversing
    changes made to e89feba997547bef550e150dc2ce063435fc4f09.

--- ecommerceManagementAPI/src/main/java/com/app/ecommerce_management_api/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.ecommerce_management_api.repository;
-
-import com.app.ecommerce_management_api.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order,Long> {
-}
--- ecommerceManagementAPI/src/main/java/com/app/ecommerce_management_api/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.app.ecommerce_management_api.service;
-
-import com.app.ecommerce_management_api.model.User;
-import org.springframework.stereotype.Service;
-
-
-public interface UserService {
-  User getUserInfoByUsername(String username);
-}
