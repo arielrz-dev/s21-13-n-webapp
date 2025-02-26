e66f07b68d21b0520add487af09013032190f9c4
arielrz-dev <ruizarmando.dev@gmail.com>
2025-02-26 12:00:46 -0300

    Added extract info methods (jwt)

--- ecommerceManagementAPI/src/main/java/com/app/ecommerce_management_api/service/UserService.java
+++ ecommerceManagementAPI/src/main/java/com/app/ecommerce_management_api/service/UserService.java
@@ -1,9 +1,18 @@
 package com.app.ecommerce_management_api.service;
 
 import com.app.ecommerce_management_api.model.User;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
 
 
 public interface UserService {
   User getUserInfoByUsername(String username);
+
+  String getUsernameFromRequest(HttpServletRequest request);
+
+  Long getUserIdFromRequest(HttpServletRequest request);
+
+  String extractTokenFromRequest(HttpServletRequest request);
+
+  User getUserFromToken(HttpServletRequest request);
 }
