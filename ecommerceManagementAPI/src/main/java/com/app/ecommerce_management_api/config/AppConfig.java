87918577370120c53101db1fbb96159c28da2c2e
arielrz-dev <ruizarmando.dev@gmail.com>
2025-03-01 17:05:59 -0300

    feat(Exception): Add global exception handling and DTO mapping improvements
    - Implemented global exception handling using @ControllerAdvice and @ExceptionHandler.
    - Created custom exceptions: ResourceNotFoundException, BadRequestException, UnauthorizedException, ForbiddenException, ConflictException, ServiceUnavailableException.
    - Added GlobalExceptionHandler to handle custom exceptions and provide consistent error responses.
    - Defined ErrorResponse class to structure error responses.
    - Updated CartItemService to throw ResourceNotFoundException when cartId is not found.

--- ecommerceManagementAPI/src/main/java/com/app/ecommerce_management_api/config/AppConfig.java
+++ ecommerceManagementAPI/src/main/java/com/app/ecommerce_management_api/config/AppConfig.java
@@ -1,6 +1,11 @@
 package com.app.ecommerce_management_api.config;
 
+import com.app.ecommerce_management_api.dto.response.CartItemResponse;
+import com.app.ecommerce_management_api.model.CartItem;
+import org.modelmapper.Condition;
+import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,11 +16,22 @@ public class AppConfig {
   public ModelMapper modelMapper(){
       ModelMapper modelMapper = new ModelMapper();
 
-      // Mapeo personalizado para User y UserDTO
-//      modelMapper.typeMap(User.class, UserDTO.class).addMappings(mapper->{
-//        mapper.map(User::getUsername,UserDTO::setUsername);
-//        mapper.map(User::getRole,UserDTO::setRole);
-//      });
+    // Condición para verificar si el valor es nulo
+    Condition<?, ?> notNull = Conditions.isNotNull();
+
+    // Definir mapeo personalizado para CartItem a CartItemResponse
+    modelMapper.addMappings(new PropertyMap<CartItem, CartItemResponse>() {
+      @Override
+      protected void configure() {
+        when(notNull).map(source.getProduct().getId(), destination.getProductId());
+        when(notNull).map(source.getProduct().getName(), destination.getProductName());
+        when(notNull).map(source.getProduct().getDescription(), destination.getProductDescription());
+        when(notNull).map(source.getProduct().getPrice(), destination.getProductPrice());
+//        when(notNull).map(source.getPrice().multiply(BigDecimal.valueOf(source.getAmount())), destination.getTotalPrice());
+        // Mapear otros campos necesarios
+      }
+    });
+
       return modelMapper;
   }
 }
